87b75b8dd3b99db48278d4301dfc37567cceccef
omariip <aboushaar.omar@gmail.com>
2022-03-12 09:24:54 +0300

    Added address to the sign up form

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,16 +8,34 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignUpPage implements OnInit {
 
-  signUp = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    phone: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    confirmPassword: new FormControl('', Validators.required)
-  })
-  constructor() { }
+  registrationForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.maxLength(100)]],
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    phone: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+    address: this.formBuilder.group({
+      street: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      province: ['', [Validators.required]],
+      postal: ['', [Validators.required, Validators.pattern(/^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i)]]
+    }),
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+
+  // signUp = new FormGroup({
+  //   name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+  //   email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+  //   phone: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+  //   password: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+  //   confirmPassword: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+  // })
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  submit(){
+    console.log(this.registrationForm.value);
+  }
 }
