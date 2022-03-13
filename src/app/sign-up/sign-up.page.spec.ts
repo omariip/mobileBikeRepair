84429f40f224d44fee7ac4f1bbaf92fc58e5b6cd
omariip <aboushaar.omar@gmail.com>
2022-03-13 03:03:08 +0300

    started adding tests

--- src/app/sign-up/sign-up.page.spec.ts
+++ src/app/sign-up/sign-up.page.spec.ts
@@ -3,14 +3,19 @@ import { IonicModule } from '@ionic/angular';
 
 import { SignUpPage } from './sign-up.page';
 
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 describe('SignUpPage', () => {
   let component: SignUpPage;
   let fixture: ComponentFixture<SignUpPage>;
 
+  let registrationPageForm: SignUpPage;
+  let form: FormGroup;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SignUpPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), FormsModule, ReactiveFormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignUpPage);
@@ -18,7 +23,21 @@ describe('SignUpPage', () => {
     fixture.detectChanges();
   }));
 
+  beforeEach(() => {
+    registrationPageForm = new SignUpPage(new FormBuilder());
+    form = registrationPageForm.getForm();
+  })
+
+  // Test: The component should be created
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Test: The name field should be invalid when empty
+  it('Name field should be invalid when empty', () =>{
+    expect(form.get('name').valid).toBeFalsy();
+  })
+
+  // Test: 
+
 });
--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
@@ -8,6 +7,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class SignUpPage implements OnInit {
 
+  
   registrationForm = this.formBuilder.group({
     name: ['', [Validators.required, Validators.maxLength(100)]],
     email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
@@ -19,10 +19,11 @@ export class SignUpPage implements OnInit {
       postal: ['', [Validators.required, Validators.pattern(/^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i)]]
     }),
     password: ['', [Validators.required, Validators.minLength(8)]],
-    confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
   });
 
-
+  getForm(): FormGroup {
+    return this.registrationForm;
+  }
   // signUp = new FormGroup({
   //   name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
   //   email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
