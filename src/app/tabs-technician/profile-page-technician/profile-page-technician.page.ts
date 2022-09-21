e8b704ce09297a44cc049480340cd0d3d2b95393
omariip <aboushaar.omar@gmail.com>
2022-09-21 03:38:25 -0400

    Implemented services and pricing feature for technician

--- src/app/tabs-technician/profile-page-technician/profile-page-technician.page.ts
+++ src/app/tabs-technician/profile-page-technician/profile-page-technician.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CurrentUserService } from 'src/app/services/current-user.service';
-
+import { IonModal } from '@ionic/angular';
+import { doc, Firestore, setDoc, updateDoc } from '@angular/fire/firestore';
+import { arrayUnion } from 'firebase/firestore';
 @Component({
   selector: 'app-profile-page-technician',
   templateUrl: './profile-page-technician.page.html',
@@ -8,21 +10,53 @@ import { CurrentUserService } from 'src/app/services/current-user.service';
 })
 export class ProfilePageTechnicianPage implements OnInit {
 
+  @ViewChild(IonModal) modal: IonModal;
   technicianInfo = null;
-  //temp = document.getElementById("editPh");
+  title;
+  description;
+  price;
+  service = null;
+
   constructor(
-    private currentUser: CurrentUserService
-    ) { 
-      
-    }
+    private currentUser: CurrentUserService,
+    private firestore: Firestore,
+    private currentUserService: CurrentUserService
+
+  ) {
+
+  }
 
   ngOnInit() {
-    this.currentUser.getCurrentUserDetails().then(data =>{
+    this.currentUser.getCurrentUserDetails().then(data => {
 
       this.technicianInfo = data;
-  })}
+      console.log(data)
+    })
+  }
 
-  editPhoneNumber() {
-    //this.temp.hidden=false;
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
+
+  async confirm() {
+
+    this.modal.dismiss('confirm');
+    await this.addService();
+    this.title = "";
+    this.description = "";
+    this.price = "";
+    this.ngOnInit();
   }
+
+  async addService() {
+    this.service = {
+      title: this.title,
+      description: this.description,
+      price: this.price
+    }
+    const tech = doc(this.firestore, "technician", this.technicianInfo.technicianId);
+    await updateDoc(tech, {
+      service: arrayUnion(this.service)
+    });
+  }
+}
