7384152c7629bad3bb3ef0480ea6c43d3b40272e
omariip <aboushaar.omar@gmail.com>
2022-09-22 02:01:25 -0400

    added edit phone functionality to tech profile

--- src/app/tabs-technician/profile-page-technician/profile-page-technician.page.ts
+++ src/app/tabs-technician/profile-page-technician/profile-page-technician.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CurrentUserService } from 'src/app/services/current-user.service';
-import { IonModal } from '@ionic/angular';
+import { IonModal, AlertController, ToastController } from '@ionic/angular';
 import { arrayRemove, doc, Firestore, setDoc, updateDoc } from '@angular/fire/firestore';
 import { arrayUnion, FieldValue } from 'firebase/firestore';
 @Component({
@@ -16,11 +16,13 @@ export class ProfilePageTechnicianPage implements OnInit {
   description = "";
   price = "";
   service = null;
+  newPhoneNumber = "";
 
   constructor(
     private currentUser: CurrentUserService,
-    private firestore: Firestore
-
+    private firestore: Firestore,
+    private alertController: AlertController,
+    private toastController: ToastController
   ) {
 
   }
@@ -31,6 +33,51 @@ export class ProfilePageTechnicianPage implements OnInit {
       this.technicianInfo = data;
       console.log(data)
     })
+  }
+
+  async presentPhoneAlert() {
+    const alert = await this.alertController.create({
+      header: 'Edit phone number',
+      message: '',
+      buttons: [{
+        text: "Confirm",
+        handler: data => {
+          if (data.newNum.match(/^[0-9]+$/) != null) {
+            this.newPhoneNumber = data.newNum
+            this.editPhoneNumber();
+          } else {
+            this.presentToast("Please enter a valid number", "danger");
+          }
+        }
+      }],
+      inputs: [{
+        type: 'text',
+        name: 'newNum',
+        placeholder: 'new number',
+        attributes: {
+          maxlength: 10
+        }
+      }]
+    })
+    await alert.present();
+  }
+
+  async presentToast(message, color) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      color: color
+    })
+    await toast.present();
+  }
+
+  async editPhoneNumber() {
+    const tech = doc(this.firestore, "technician", this.technicianInfo.technicianId);
+    await updateDoc(tech, {
+      technicianPhone: this.newPhoneNumber
+    })
+    this.ngOnInit();
+    this.presentToast("Successfully edited phone number", "success");
   }
 
   cancel() {
