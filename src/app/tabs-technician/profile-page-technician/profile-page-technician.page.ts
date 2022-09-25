3e1a9854217e94f15fad9450eb6f27be5ac004ae
omariip <aboushaar.omar@gmail.com>
2022-09-25 05:12:05 -0400

    added location edit in tech profile

--- src/app/tabs-technician/profile-page-technician/profile-page-technician.page.ts
+++ src/app/tabs-technician/profile-page-technician/profile-page-technician.page.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CurrentUserService } from 'src/app/services/current-user.service';
-import { IonModal, AlertController, ToastController } from '@ionic/angular';
+import { IonModal, AlertController, ToastController, IonInput } from '@ionic/angular';
 import { arrayRemove, doc, Firestore, setDoc, updateDoc } from '@angular/fire/firestore';
 import { arrayUnion, FieldValue } from 'firebase/firestore';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { findAddressNumber, findStreet, findCity, findState, findZipCode } from 'src/utils/address-utils';
+declare var google;
+
 @Component({
   selector: 'app-profile-page-technician',
   templateUrl: './profile-page-technician.page.html',
@@ -10,7 +14,10 @@ import { arrayUnion, FieldValue } from 'firebase/firestore';
 })
 export class ProfilePageTechnicianPage implements OnInit {
 
-  @ViewChild(IonModal) modal: IonModal;
+  @ViewChild('modal') modal: IonModal;
+  @ViewChild('modal2') modal2: IonModal;
+  @ViewChild('autocomplete', { static: false }) autocomplete: IonInput;
+  
   technicianInfo = null;
   title = "";
   description = "";
@@ -22,10 +29,19 @@ export class ProfilePageTechnicianPage implements OnInit {
     private currentUser: CurrentUserService,
     private firestore: Firestore,
     private alertController: AlertController,
-    private toastController: ToastController
+    private toastController: ToastController,
+    private formBuilder: FormBuilder
   ) {
-
   }
+
+  registrationForm = this.formBuilder.group({
+    address: this.formBuilder.group({
+      street: ['', [Validators.required, Validators.maxLength(100)]],
+      city: ['', [Validators.required, Validators.maxLength(100)]],
+      province: ['', [Validators.required]],
+      postal: ['', [Validators.required, Validators.pattern(/^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i)]]
+    })
+  })
 
   ngOnInit() {
     this.currentUser.getCurrentUserDetails().then(data => {
@@ -80,28 +96,28 @@ export class ProfilePageTechnicianPage implements OnInit {
     this.presentToast("Successfully edited phone number", "success");
   }
 
-  cancel() {
+  cancelService() {
     this.modal.dismiss(null, 'cancel');
     this.title = "";
     this.description = "";
     this.price = "";
   }
 
-  async confirm() {
+  cancelAddress() {
+    this.modal2.dismiss(null, 'cancel');
+  }
 
-    this.modal.dismiss('confirm');
+  async confirmService() {
+
+    this.modal2.dismiss('confirm');
     await this.addService();
-    this.title = "";
-    this.description = "";
-    this.price = "";
     this.ngOnInit();
   }
 
-  async delete(i){
-    const tech = doc(this.firestore, "technician", this.technicianInfo.technicianId);
-    await updateDoc(tech, {
-      service: arrayRemove(this.technicianInfo.service[i])
-    })
+  async confirmAddress() {
+
+    this.modal2.dismiss('confirm');
+    await this.editAddress();
     this.ngOnInit();
   }
 
@@ -115,5 +131,46 @@ export class ProfilePageTechnicianPage implements OnInit {
     await updateDoc(tech, {
       service: arrayUnion(this.service)
     });
+  }
+
+  async editAddress(){
+    const tech = doc(this.firestore, "technician", this.technicianInfo.technicianId);
+    await updateDoc(tech, {
+      technicianAddress: this.registrationForm.get('address').value
+    })
+  }
+
+  async delete(i) {
+    const tech = doc(this.firestore, "technician", this.technicianInfo.technicianId);
+    await updateDoc(tech, {
+      service: arrayRemove(this.technicianInfo.service[i])
+    })
+    this.ngOnInit();
+  }
+
+  
+
+  setAddress(place) {
+    const addressForm = this.registrationForm.get('address');
+
+    addressForm.get('street').setValue(findAddressNumber(place.address_components) + " " + findStreet(place.address_components));
+    addressForm.get('city').setValue(findCity(place.address_components));
+    addressForm.get('province').setValue(findState(place.address_components));
+    addressForm.get('postal').setValue(findZipCode(place.address_components));
+  }
+
+  editAddressClicked(){
+    setTimeout(() => {
+      var options = {
+        componentRestrictions: { country: "ca" }
+      };
+      this.autocomplete.getInputElement().then((ref: any) => {
+        const autocomplete = new google.maps.places.Autocomplete(ref, options);
+  
+        autocomplete.addListener('place_changed', () => {
+          this.setAddress(autocomplete.getPlace());
+        })
+      })
+    }, 500)
   }
 }
